1189d4f4a0ac27451b451cf80cf2a81c719b6410
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-14 10:27:41 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = 0;
+            string message = "Hello World";
+            Console.WriteLine(message);
+        }
+    }
+}
